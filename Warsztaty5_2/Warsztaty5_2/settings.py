20c6c20d50277d53bde19a079463547dba45e084
ibm <dawid.figiel@gmail.com>
2018-12-30 15:32:43 +0100

    Add backgroud image

--- Warsztaty5_2/Warsztaty5_2/settings.py
+++ Warsztaty5_2/Warsztaty5_2/settings.py
@@ -123,9 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
